2889276460b84239b7e110fa4835f44267e13cde
Carson Scott <carsonjscott14@gmail.com>
2019-10-22 14:48:36 -0400

    added repository url to setup

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     author='Carson Scott',
     description='A Self-Organizing Map for Multiclass Classification.',
     author_email='carsonjscott14@gmail.com',
-    url='https://github.com/CarsonScott/self-organizing-map',
+    url='git@github.com:CarsonScott/self-organizing-map.git',
     packages=['som'],
 )
+

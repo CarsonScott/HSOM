04c2f8262cf8fba8897a5f52602669cfadad9f68
Carson Scott <carsonjscott14@gmail.com>
2019-10-26 23:10:47 -0400

    Update self_organizing_memory.py

--- examples/self_organizing_memory.py
+++ examples/self_organizing_memory.py
@@ -5,6 +5,8 @@ input_size = 100
 node_count = 5
 learning_rate = 0.05
 weight_range = (0.01, 0.03)
+layer_sizes = [20, 15, 10, 5, 2]
+link_percentages = [0.2, 0.2, 0.2, 0.2, 0.75]
 
 # Create hierarchical layers of size 20, 15, 10, 5, and 2
 self_organizing_memory = SelfOrganizingMemory(
@@ -12,8 +14,8 @@ self_organizing_memory = SelfOrganizingMemory(
   node_count=node_count, 
   input_size=input_size, 
   weight_range=weight_range, 
-  layer_sizes=[20, 15, 10, 5, 2], 
-  link_percentages=[0.2, 0.2, 0.2, 0.2, 0.75])
+  layer_sizes=layer_sizes, 
+  link_percentages=link_percentages)
   
 # Create a set of sparse samples
 samples=[]

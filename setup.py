ea00a1265d29ec67f8e7c87f25198cd5a3996bee
Carson Scott <carsonjscott14@gmail.com>
2019-12-18 21:40:03 -0500

    renamed source folder

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 
 setup(
-    name='som',
+    name='hsom',
     version='1.0',
     author='Carson Scott',
-    description='A Self-Organizing Map for Multiclass Classification.',
+    description='Hierarchical Self-Organizing Map for Multiclass Classification.',
     author_email='carsonjscott14@gmail.com',
     url='https://github.com/CarsonScott/self-organizing-map.git',
-    packages=['som'],
+    packages=['hsom'],
 )
 
